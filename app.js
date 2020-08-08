9f919c76e3a5fdb74b3898f4ea4ed46f02a59acb
Courtney Berry <64494409+courtneyberry27@users.noreply.github.com>
2020-08-07 20:19:23 -0500

    fixes error handlers

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 const books = require('./routes/books');
 const { sequelize } = require('./db/index');
-const otherErrors = require('./middleware/otherErrors');
-const error404 = require('./middleware/error404');
 
 //create port number
 const port = process.env.PORT || 3000;
@@ -20,22 +18,33 @@ app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
 //use body-parser
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //routes
 app.use('/', routes);
 app.use('/books', books);
 
-//errors
-app.use(otherErrors);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    const err = new Error();
+    err.status = 404;
+    err.message = "- Sorry, this page does not exist"
+    res.render('page-not-found');
+    next(err);
+});
 
-//404
-app.use('/error404', error404);
-
-
+// error handler
+app.use(function(err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 //set up port listener and sync
 sequelize.sync()
-  .then(() => {
-    app.listen(port, () => console.log('The application is running on port 3000!'));
-  });
+    .then(() => {
+        app.listen(port, () => console.log('The application is running on port 3000!'));
+    });
+
+module.exports = app;
